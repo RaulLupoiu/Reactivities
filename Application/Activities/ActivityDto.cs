6b8297cc7b3c35e0255024fbc28977c48c22ec68
Raul <lupoiuraul20@gmail.com>
2020-03-27 18:59:38 +0200

    final14

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
-namespace Domain
+namespace Application.Activities
 {
-    public class Activity
+    public class ActivityDto
     {
-
+        
      public Guid Id{get; set; }
      public string Title {get; set; }
      public string Description {get; set; }
@@ -13,6 +14,9 @@ namespace Domain
      public DateTime Date { get; set; }
      public string City{ get; set; }
      public string Venue { get; set; }
-     public virtual ICollection<UserActivity> UserActivities {get; set;}
-    } 
-}  
+     
+     [JsonProperty("attendees")]    
+      public ICollection<AttendeeDto> UserActivities {get; set;}
+
+    }
+}
